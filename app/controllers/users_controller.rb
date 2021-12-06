ead7055fff6c5510906b068783a4bd04c6b70e0e
fyky <fuyu12dmb25ms@gmail.com>
2021-12-06 09:42:59 +0000

    [Add]deviseログイン・新規登録

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def unsubscribe
+  end
+  
+  def host
+  end
+  
+  def attend
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
+
+  devise_for :users
+  resource :users, only: [:index, :edit, :update] do
+    collection do
+      get "unsubscribe"
+      patch "withdraw"
+      get "host"
+      get "attend"
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
