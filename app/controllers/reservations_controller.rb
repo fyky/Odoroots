f1c03f0cba43551a55374244b14ca959651b2eb5
fyky <fuyu12dmb25ms@gmail.com>
2021-12-08 08:28:55 +0000

    [Add]イベント予約確認画面・完了画面遷移

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,13 +1,32 @@
 class ReservationsController < ApplicationController
   def new
+    @reservation = Reservation.new
     @event = Event.find(params[:event_id])
-
+    @reservation.user_id = current_user.id
   end
 
   def confirm
+    @reservation = Reservation.new(reservation_params)
+
+    # @reservation = Reservation.find(params[:id])
+    @event = Event.find(params[:event_id])
+    if @event.invalid? #入力項目に空のものがあれば入力画面に遷移
+      render :new
+    end
+
+    # binding.irb
   end
 
   def create
+    @event = Event.find(params[:event_id])
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      redirect_to complete_event_reservations_path
+    else
+      render :new
+
+    end
   end
 
   def complete
@@ -15,4 +34,11 @@ class ReservationsController < ApplicationController
 
   def index
   end
+
+
+  private
+    def reservation_params
+      params.require(:reservation).permit(:comment, :user_id, :event_id)
+    end
+
 end
