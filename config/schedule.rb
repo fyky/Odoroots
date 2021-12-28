1d5db426128f25039ab2cdb0b1c20d5ab014776f
fyky <fuyu12dmb25ms@gmail.com>
2021-12-28 15:14:53 +0000

    [Fix]whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,7 +21,7 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/environment')
 # 実行環境を指定する
-# set :environment, Rails.env.to_sym
+set :environment, Rails.env.to_sym
 # 実行logの出力先
 set :output, "#{Rails.root.to_s}/log/cron.log"
 
