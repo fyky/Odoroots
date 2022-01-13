78576b5c53504c44cafed1e991f897f18d4731c8
fyky <fuyu12dmb25ms@gmail.com>
2022-01-13 05:18:31 +0000

    [Fix]js切り分け

--- app/assets/javascripts/event_map.js
+++ app/assets/javascripts/event_map.js
@@ -1,6 +1,29 @@
-// window.onload = function () {
-//   const event = document.getElementById('event').value
-//   const latitude = document.getElementById('latitude').value
-//   const longitude = document.getElementById('longitude').value
-//   // alert(longitude);
-// }
+function initMap() {
+
+    var latitude = document.getElementById('latitude').value
+    var longitude = document.getElementById('longitude').value
+    var full_address = document.getElementById('full_address').value
+
+    var test = new google.maps.LatLng(latitude, longitude)
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: test
+    });
+    var transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(map);
+
+    var contentString = "住所："+full_address;
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: test,
+        map: map,
+        title: contentString
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
