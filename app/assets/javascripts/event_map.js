c045e167370e240636179a81707b2d3d025e2807
fyky <fuyu12dmb25ms@gmail.com>
2022-01-14 05:47:37 +0000

    [Fix]地図表示

--- app/assets/javascripts/event_map.js
+++ app/assets/javascripts/event_map.js
@@ -1,30 +1,32 @@
-function initMap() {
+$(document).on('turbolinks:load', function() {
+    function initMap() {
 
-    // var latitude = document.getElementById('latitude').value
-    // var longitude = document.getElementById('longitude').value
-    // var full_address = document.getElementById('full_address').value
+        // var latitude = document.getElementById('latitude').value
+        // var longitude = document.getElementById('longitude').value
+        // var full_address = document.getElementById('full_address').value
 
-    // var test = new google.maps.LatLng(latitude, longitude)
-    var test ={lat: gon.event.latitude, lng: gon.event.longitude};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: test
-    });
-    var transitLayer = new google.maps.TransitLayer();
-    transitLayer.setMap(map);
+        // var test = new google.maps.LatLng(latitude, longitude)
+        var test ={lat: gon.event.latitude, lng: gon.event.longitude};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: test
+        });
+        var transitLayer = new google.maps.TransitLayer();
+        transitLayer.setMap(map);
 
-    var contentString = "住所："+ gon.event.full_address;
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
+        var contentString = "住所："+ gon.event.full_address;
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
 
-    var marker = new google.maps.Marker({
-        position: test,
-        map: map,
-        title: contentString
-    });
+        var marker = new google.maps.Marker({
+            position: test,
+            map: map,
+            title: contentString
+        });
 
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
-}
+        marker.addListener('click', function() {
+            infowindow.open(map, marker);
+        });
+    }
+});
