64d4fd8a983fde06a562533d73d741d42940d6bd
fyky <fuyu12dmb25ms@gmail.com>
2022-01-25 09:17:36 +0000

    [Add]replyカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_072909) do
+ActiveRecord::Schema.define(version: 2022_01_25_090532) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_12_19_072909) do
     t.integer "event_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "reply_id"
   end
 
   create_table "events", force: :cascade do |t|
